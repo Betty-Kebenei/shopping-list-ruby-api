e688e3c0c658f929b389453292ebdde445d1cdb8
Betty-Kebenei <kebeneibetty13@gmail.com>
2018-06-13 15:52:48 +0300

    Fetch all shopping lists

--- app/controllers/shopping_lists_controller.rb
+++ app/controllers/shopping_lists_controller.rb
@@ -1,2 +1,7 @@
 class ShoppingListsController < ApplicationController
+    def index
+        @shopping_lists = ShoppingList.all
+        render json: @shopping_lists
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :shopping_lists do
+    resources :shopping_items
+  end
 end
