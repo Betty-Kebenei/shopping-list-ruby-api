adaa7fb072ccc13799bd2a4089d08aab4908acaf
Betty-Kebenei <kebeneibetty13@gmail.com>
2018-06-13 12:28:45 +0300

    Add tests for the shopping lists API

--- spec/factories/shopping_lists.rb
+++ spec/factories/shopping_lists.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
     factory :shopping_list do
-      title { Faker::Lorem.word }
+      title { Faker::Lorem.words }
       created_by { Faker::Number.number(10) }
     end
   end
