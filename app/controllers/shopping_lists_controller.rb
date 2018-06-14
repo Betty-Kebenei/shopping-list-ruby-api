13cb0cf808aeb162ef6dd0d0bc7217f1b77d560c
Betty-Kebenei <kebeneibetty13@gmail.com>
2018-06-14 15:06:33 +0300

    Fetch shopping list by id

--- app/controllers/shopping_lists_controller.rb
+++ app/controllers/shopping_lists_controller.rb
@@ -1,7 +1,19 @@
 class ShoppingListsController < ApplicationController
     def index
         @shopping_lists = ShoppingList.all
-        render json: @shopping_lists
+        render json: @shopping_lists, status: :ok
+    end
+
+    def show
+        @shopping_list = ShoppingList.find_by(id: params[:id])
+
+        response = if @shopping_list 
+            { status: :ok , json: { shopping_list: @shopping_list }.to_json }
+        else
+            { status: :not_found }
+        end
+
+        render response
     end
 
 end
